13ab1caefbc6d15f476264560d165c2cdafc3b6f
thirukkumaransv <52705136+thirukkumaransv@users.noreply.github.com>
2019-07-12 16:52:00 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,9 +1,9 @@
-num1 = float(input(""))
-num2 = float(input(""))
-num3 = float(input(""))
-if(num1 > num2) and (num1 > num3):
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+if(num1 >= num2) and (num1 >= num3):
   largest = num1
-elif(num2 > num1) and (num2 > num3):
+elif(num2 >= num1) and (num2 >= num3):
   largest = num2
 else:
   largest = num3
